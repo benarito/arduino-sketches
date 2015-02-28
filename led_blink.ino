f97d5a2e7aa5e5a3c70fbabe6efde4b953e518a7
Benson Njogu <bentric@yahoo.com>
2015-02-28 12:28:57 +0300

    added blinking led sketch

---
+++ led_blink.ino
@@ -0,0 +1,14 @@
+// blinking LED
+
+int ledPin = 10;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
+}
